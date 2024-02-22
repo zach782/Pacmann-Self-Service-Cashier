63aa6bfa89e6cc2115bd13fde224e1f1bce9189b
zach782 <handibaleng45@gmail.com>
2024-02-22 10:28:15 +0700

    update

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 
-
-
 # Latar Belakang Problems
 Andi adalah seorang pemilik supermarket besar di salah satu kota di Indonesia. Andi memiliki rencana unutk melakukan perbaikan proses bisnis, yaitu Andi akan membuat sistem kasir yang self-service di supermarket miliknya. Sehingga customer bisa langsung memasukkan item yang dibeli, jumlah item yang dibeli, dan harga item yang dibeli, serta fitur yang lain.
 Harapan Andi, customer yang tidak berada di kota tersebut bisa membeli barang dari supermarket tersebut. Setelah Andi melakukan riset, ternyata Andi memiliki masalah, yaitu Andi membutuhkan Programmer untuk membuatkan fitur-fitur agar sistem self-service di supermarket itu bisa berjalan dengan lancar.
--- modul_main.py
+++ modul_main.py
@@ -6,28 +6,28 @@ class Transaction:
     """
     class Transaction digunakan sebagai Template untuk membuat objek id transaksi
     """
-    """
-    Fungsi init memiliki atribut objek
-    1. no sebagai nomor item dalam tabel
-    2. nama sebagai nama item
-    3. jumlah sebagai jumlah item yang ingin dibeli
-    4. harga sebagai harga per item
-    5. total sebagai harga total dari jumlah dikali harga per item
-    6. list keranjang untuk menampung identitas barang
-    """
     def __init__(self, inputNo=None, inputNamaItem=None, inputJumlah=None, inputHarga=None):
+        """
+        Fungsi init memiliki atribut objek
+        1. no sebagai nomor item dalam tabel
+        2. nama sebagai nama item
+        3. jumlah sebagai jumlah item yang ingin dibeli
+        4. harga sebagai harga per item
+        5. total sebagai harga total dari jumlah dikali harga per item
+        6. list keranjang untuk menampung identitas barang
+        """
         self.no = inputNo
         self.nama = inputNamaItem
         self.jumlah = inputJumlah
         self.harga = inputHarga
         self.total = 0
         self.keranjang = []
-    """
-    Fungsi add_item untuk menambahkan nomor tabel, nama item, jumlah item, 
-    harga item, harga total kedalam list keranjang, harga total didapat dari
-    jumlah item dikali harga per item
-    """
     def add_item(self, no, nama, jumlah, harga):
+        """
+        Fungsi add_item untuk menambahkan nomor tabel, nama item, jumlah item, 
+        harga item, harga total kedalam list keranjang, harga total didapat dari
+        jumlah item dikali harga per item
+        """
         try:
             harga_total = jumlah * harga
             tambah = [no, nama, jumlah, harga, harga_total]
@@ -38,12 +38,12 @@ class Transaction:
             print(tabulate(self.keranjang, headers=headers, tablefmt="grid"))
         except NameError:
             print("Masukkan nama yang valid !!!")
-    """
-    Fungsi update_item_name memiliki paramater nama_lama dan nama_baru,
-    nama_lama digunakan untuk mengupdate nama item berdasarkan nama item yang ingin diubah
-    dan nama_baru akan digunakan untuk nama terbaru pada list keranjang 
-    """
     def update_item_name(self, nama_lama, nama_baru):
+        """
+        Fungsi update_item_name memiliki paramater nama_lama dan nama_baru,
+        nama_lama digunakan untuk mengupdate nama item berdasarkan nama item yang ingin diubah
+        dan nama_baru akan digunakan untuk nama terbaru pada list keranjang 
+        """
         try:
             item_ditemukan = False
             for indeks, barang in enumerate(self.keranjang):
@@ -58,12 +58,12 @@ class Transaction:
                 print(tabulate(self.keranjang, headers=headers, tablefmt="grid"))
         except Exception as e:
             print(f"Terjadi kesalahan saat mengubah nama item: {e}")
-    """
-    Fungsi update_item_qty memiliki parameter nama_barang, jumlah_baru 
-    digunakan untuk mengubah jumlah item yang ingin dibeli berdasarkan
-    nama item yang ingin diubah jumlahnya
-    """
     def update_item_qty(self, nama_barang, jumlah_baru):
+        """
+        Fungsi update_item_qty memiliki parameter nama_barang, jumlah_baru 
+        digunakan untuk mengubah jumlah item yang ingin dibeli berdasarkan
+        nama item yang ingin diubah jumlahnya
+        """
         try:
             for index, barang in enumerate(self.keranjang):
                 if barang[1] == nama_barang:
@@ -81,12 +81,12 @@ class Transaction:
         headers = ["No", "Nama Barang", "Jumlah", "Harga/Item", "Harga Total"]
         print(tabulate(self.keranjang, headers=headers, tablefmt="grid"))
         print("")
-    """
-    Fungsi update_item_price memiliki parameter nama_barang, harga_baru 
-    digunakan untuk mengubah harga per item yang ingin dibeli berdasarkan
-    nama item yang ingin diubah harga per itemnya
-    """
     def update_item_price(self, nama_barang, harga_baru):
+        """
+        Fungsi update_item_price memiliki parameter nama_barang, harga_baru 
+        digunakan untuk mengubah harga per item yang ingin dibeli berdasarkan
+        nama item yang ingin diubah harga per itemnya
+        """
         try:
             for indeks, barang in enumerate(self.keranjang):
                 if barang[1] == nama_barang:
@@ -102,11 +102,11 @@ class Transaction:
         print("\nIsi keranjang Setelah Harga / Item diubah:")
         headers = ["No", "Nama Barang", "Jumlah", "Harga", "Harga Total"]
         print(tabulate(self.keranjang, headers=headers, tablefmt="grid"))
-    """
-    Fungsi delete_item memiliki parameter nama_item yang akan digunakan
-    untuk menghapus item berdasarkan nama item yang ingin dihapus
-    """
     def delete_item(self, nama_item):
+        """
+        Fungsi delete_item memiliki parameter nama_item yang akan digunakan
+        untuk menghapus item berdasarkan nama item yang ingin dihapus
+        """
         try:
             for barang in self.keranjang:
                 if barang[1] == nama_item:
@@ -120,12 +120,12 @@ class Transaction:
         print("\nIsi keranjang Setelah Item dihapus:")
         headers = ["No", "Nama Barang", "Jumlah", "Harga", "Harga Total"]
         print(tabulate(self.keranjang, headers=headers, tablefmt="grid"))
-    """
-    Fungsi reset_transaction digunakan untuk mereset atau menghapus semua
-    item yang sudah dimasukan ke list keranjang mulai dari nomor item, nama item, 
-    jumlah, harga/item, harga total
-    """
     def reset_transaction(self):
+        """
+        Fungsi reset_transaction digunakan untuk mereset atau menghapus semua
+        item yang sudah dimasukan ke list keranjang mulai dari nomor item, nama item, 
+        jumlah, harga/item, harga total
+        """
         self.keranjang = []
         self.total = 0
         print("")
@@ -133,11 +133,11 @@ class Transaction:
         print("Isi keranjang:")
         headers = ["No", "Nama Barang", "Jumlah", "Harga/Item", "Harga Total"]
         print(tabulate(self.keranjang, headers=headers, tablefmt="grid"))
-    """
-    Fungsi check_order digunakan untuk mengecek identitas item mulai dari
-    nama, jumlah, harga per item
-    """
     def check_order(self):
+        """
+        Fungsi check_order digunakan untuk mengecek identitas item mulai dari
+        nama, jumlah, harga per item
+        """
         for indeks, barang in enumerate(self.keranjang):
             try:
                 if not barang[1]:
@@ -151,12 +151,12 @@ class Transaction:
             except IndexError as e:
                 print(f"Item {indeks + 1} tidak lengkap: {e}")
         print("Pengecekan selesai\n")
-    """
-    Fungsi total_price digunakan untuk melihat total harga dari semua
-    item yang akan dibeli, jika total harga memenuhi ketentuan diskon,
-    maka total harga akan dapat potongan diskon
-    """
     def total_price(self):
+        """
+        Fungsi total_price digunakan untuk melihat total harga dari semua
+        item yang akan dibeli, jika total harga memenuhi ketentuan diskon,
+        maka total harga akan dapat potongan diskon
+        """
         diskon = 0
         total_harga = self.total
         try:
@@ -185,13 +185,13 @@ class Transaction:
         print("")
         print(f"Total Belanja Anda Sebesar: Rp {total_harga}")
         print("")
-    """
-    Fungsi menu digunakan sebagai interaksi antara customer
-    dan sistem supermarket, customer dapat melakukan berbagai 
-    operasi seperti menambahkan item, memperbarui detail item, 
-    menghapus item, memeriksa pesanan, dan menghitung total harga
-    """
     def menu(self):
+        """
+        Fungsi menu digunakan sebagai interaksi antara customer
+        dan sistem supermarket, customer dapat melakukan berbagai 
+        operasi seperti menambahkan item, memperbarui detail item, 
+        menghapus item, memeriksa pesanan, dan menghitung total harga
+        """
         no = 0
         while True:
             no = no + 1
@@ -239,5 +239,4 @@ class Transaction:
 
 # trnsct_123 = Transaction()
 # trnsct_123.menu()
-
 print("Program Selesai\n")
